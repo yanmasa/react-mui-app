ed1b31e56a7072a1d90e38f17b0d4f0b7c17f324
yanmasa <ueda3ban@gmail.com>
2022-05-19 13:27:44 +0900

    Grobalスタイルの変更

---
+++ src/theme/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0052cc',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+    background: {
+      default: '#969090',
+      paper: '#fbf7f7',
+    },
+  },
+});
+
+export default theme;
