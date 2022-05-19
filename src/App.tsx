12f0a3f137ce177fb8797fb638f9ae8ae8d8e8ea
yanmasa <ueda3ban@gmail.com>
2022-05-19 11:11:10 +0900

    Gridシステムの確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-
+import React from "react";
+import Button from "@mui/material/Button";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={6} md={8}>
+        <Button variant="contained" color="success">
+          Hello World
+        </Button>
+      </Grid>
+      <Grid item xs={6} md={4}>
+        <Button variant="text" color="error">
+          Hello World
+        </Button>
+      </Grid>
+      <Grid item xs={6} md={4}>
+        <Button variant="contained" color="success">
+          Hello World
+        </Button>
+      </Grid>
+      <Grid item xs={6} md={8}>
+        <Button variant="contained" color="success">
+          Hello World
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
